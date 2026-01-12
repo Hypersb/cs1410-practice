3b1572bed00f221270d697d97d79006cf1925756
Subhanjan Bikram KC <sbkchoni@gmail.com>
2026-01-12 20:56:22 +0000

    secondcode

---
+++ src/secondcode.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double c, f;
+    cout << "temperature in celcius: " << endl;
+    cin >> c;
+
+     f = 9.0 * c / 5.0 + 32;
+
+    cout << "temperature in degree: " << f << endl;
+    return 0;
+}
