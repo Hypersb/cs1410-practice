ccc7bf8ee00abab0430ed468326a886781ab9a94
Subhanjan Bikram KC <sbkchoni@gmail.com>
2026-01-16 20:29:17 +0000

    quiz final submission

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main(){
-    // TODO
-    std::cout << "Hello World!" << std::endl;
-    
+    // Assignment 1
+    std::cout << "Name: Subhanjan Bikram K C" << std::endl;
+    std::cout << "Github Username: Subhanjan Bikram K C" << std::endl;
     return 0;
 }
